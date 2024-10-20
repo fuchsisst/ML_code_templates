ec6fc69c727a6026ba36c8221fab9763bc3c9da8
Vitalina Stepnikova <72044379+fuchsisst@users.noreply.github.com>
2024-10-20 20:53:24 +0300

    Update Simple Linear Regression.py

--- Simple Linear Regression.py
+++ Simple Linear Regression.py
@@ -35,3 +35,10 @@ plt.title('Salary vs Experience (Test set)')
 plt.xlabel('Years of Experience')
 plt.ylabel('Salary')
 plt.show()
+
+# Making a single prediction (for example the salary of an employee with 12 years of experience)
+print(regressor.predict([[12]]))
+
+# Getting the final linear regression equation with the values of the coefficients
+print(regressor.coef_)
+print(regressor.intercept_)
